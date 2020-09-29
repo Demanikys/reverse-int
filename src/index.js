3d7c19a5d7113929414821c481bc60a082c71756
Demanikys <snopok.stanislav@mail.ru>
2020-09-29 22:33:57 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    var result = '';
+    var i = 0;
+    var str = n.toString();
+    if (str[0] === '-') {
+        for (i = 1; i < str.length; i++) {
+            result = `${str[i]}${result}`;
+        }
+    } else {
+        for (i; i < str.length; i++) {
+            result = `${str[i]}${result}`;
+        };
+    };
+    return result;
 }
